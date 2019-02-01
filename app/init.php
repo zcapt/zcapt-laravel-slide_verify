6e36cf854142be52f5bd058d3e877af13567c25d
jonnyy <caijishiwo@gmail.com>
2019-02-01 08:40:54 -0500

    create method in Eloquent to associate one model with another

--- app/init.php
+++ app/init.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class init extends Model
 {
     protected $fillable = ['authID', 'x', 'y', 'verified'];
+
+
+    public function largeImg(){
+        return $this->hasmany('App\getLargeImage.php');
+    }
 }
